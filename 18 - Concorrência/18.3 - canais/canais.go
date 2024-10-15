a6259c372e4e7fbcb1dd2be335d4edb31892bd5a
Leonardo <133729985+Th3Lover@users.noreply.github.com>
2024-10-15 12:21:27 -0300

    Aula sobre canais
    Terminar aula 18.3

---
+++ 18 - Concorrência/18.3 - canais/canais.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal := make(chan string)
+	go escrever("Olá Mundo", canal)
+
+	mensagem := <-canal
+	fmt.Println(mensagem)
+}
+
+func escrever(texto string, canal chan string) {
+	for i := 0; i < 5; i++ {
+		canal <- texto
+		time.Sleep(time.Second)
+	}
+}
